247f99249c767c521110b4b9648616d225ebcf65
s27030 <s27030@pjwstk.edu.pl>
2024-04-21 19:56:19 +0200

    UpdateAnimalDTO.cs update

--- Animal/UpdateAnimalDTO.cs
+++ Animal/UpdateAnimalDTO.cs
@@ -1,9 +1,15 @@
-﻿namespace WebApplication1.Animal;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Animal;
 
 public class UpdateAnimalDTO
 {
+    [Required]
     public string Name { get; set; }
-    public string Description { get; set; }
+    
+    public string? Description { get; set; }
+    [Required]
     public string Category { get; set; }
+    [Required]
     public string Area { get; set; }
 }
